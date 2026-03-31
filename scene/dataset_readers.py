715c3c643092520228bfb02406a5a5538334da77
wowo0709 <wjsduddn0709@naver.com>
2026-03-31 11:32:47 +0000

    [DEBUG] Fix image type error

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -227,7 +227,7 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
 
             norm_data = im_data / 255.0
             arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
-            image = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+            image = Image.fromarray(np.array(arr*255.0, dtype=np.uint8), "RGB")
 
             fovy = focal2fov(fov2focal(fovx, image.size[0]), image.size[1])
             FovY = fovy 
@@ -291,7 +291,7 @@ def readCamerasFromLerfTransforms(path, transformsfile, white_background, extens
 
             norm_data = im_data / 255.0
             arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
-            image = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+            image = Image.fromarray(np.array(arr*255.0, dtype=np.uint8), "RGB")
 
             fovx = 2 * np.arctan(frame['w'] / (2 * frame['fl_x']))
             fovy = 2 * np.arctan(frame['h'] / (2 * frame['fl_y']))
